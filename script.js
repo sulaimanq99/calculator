40f19ff2009fad179bc91ec9a7326ce36a32fea8
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-12-12 22:20:54 +0000

    created js file, added way to change display value

---
+++ script.js
@@ -0,0 +1,15 @@
+const num = document.getElementsByClassName('num')
+const display = document.getElementById('d')
+
+
+
+
+num[1].addEventListener('click', function (e) {
+    display.textContent = e.target.innerHTML
+  });
+
+
+function button_select(e){
+
+
+}
