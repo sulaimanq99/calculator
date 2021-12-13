d6e9d5389b9d80f8471115fa74ef5d5eb37be8f5
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-12-13 19:01:06 +0000

    can do basic divison

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 const num = Array.from(document.getElementsByClassName('num'))
 const display = document.getElementById('d')
-const divide = document.getElementById('divide')
+const operations = Array.from(document.getElementsByClassName('operation'))
+const equals = document.getElementById('equal')
+let opType = ''
+let n1 = NaN
+let n2 = NaN
+let operatorSelected = false
 
-const n1 = parseInt(display.textContent)
 
-num.forEach(num => num.addEventListener('click', clicker ))
+num.forEach(num => num.addEventListener('click', numSelect))
 
+operations.forEach(operations => operations.addEventListener('click',opSelect))
 
+equals.addEventListener('click', operate)
 
-
-function clicker (e) {
-    console.log(e.target.innerHTML)
-    display.textContent = display.textContent +  e.target.innerHTML
-    const n1 = parseInt(display.textContent)
-    console.log(n1)
+function operate() {
+  console.log(opType)
+  if (opType == 'divide'){
+    const result = n1/n2
+    console.log('result')
+    display.textContent = result
   }
+}
 
+function opSelect(e){
+  display.textContent = ''
+  operatorSelected = true
+  console.log(e.target.id)
+  opType = e.target.id
+  console.log(opType)
+}
 
-
+function numSelect (e) {
+    display.textContent = display.textContent +  e.target.innerHTML
+    if (operatorSelected == false){
+      n1 = parseFloat(display.textContent)
+      console.log("n1: "+n1)
+    }
+    else {
+      n2 = parseFloat(display.textContent)
+      console.log("n2: "+n2)
+    }
+  }
