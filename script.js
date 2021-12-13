2a54287e56a100246de72463e85dd518b01a612e
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-12-13 18:21:10 +0000

    display updates and value is stored

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const num = document.getElementsByClassName('num')
+const num = Array.from(document.getElementsByClassName('num'))
 const display = document.getElementById('d')
+const divide = document.getElementById('divide')
+
+const n1 = parseInt(display.textContent)
+
+num.forEach(num => num.addEventListener('click', clicker ))
 
 
 
 
-num[1].addEventListener('click', function (e) {
-    display.textContent = e.target.innerHTML
-  });
+function clicker (e) {
+    console.log(e.target.innerHTML)
+    display.textContent = display.textContent +  e.target.innerHTML
+    const n1 = parseInt(display.textContent)
+    console.log(n1)
+  }
 
 
-function button_select(e){
 
-
-}
