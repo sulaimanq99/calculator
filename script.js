3b599f34416868ed91274f04a50d8f4a436db0b6
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-12-13 22:22:43 +0000

    completed all operations except clear

--- script.js
+++ script.js
@@ -2,43 +2,88 @@ const num = Array.from(document.getElementsByClassName('num'))
 const display = document.getElementById('d')
 const operations = Array.from(document.getElementsByClassName('operation'))
 const equals = document.getElementById('equal')
+const sign = document.getElementById('sign')
 let opType = ''
 let n1 = NaN
 let n2 = NaN
 let operatorSelected = false
+let temp = ''
 
 
 num.forEach(num => num.addEventListener('click', numSelect))
 
-operations.forEach(operations => operations.addEventListener('click',opSelect))
+operations.forEach(operations => operations.addEventListener('click', opSelect))
 
 equals.addEventListener('click', operate)
 
+sign.addEventListener('click', changeSign)
+
 function operate() {
   console.log(opType)
+  operatorSelected = false
+  let result = ''
   if (opType == 'divide'){
-    const result = n1/n2
-    console.log('result')
-    display.textContent = result
+    result = n1/n2
   }
+  else if (opType == 'times'){
+    result = n1*n2
+  }
+  else if (opType == 'sub'){
+    result = n1-n2
+  }
+  else if (opType == 'add'){
+    result = n1+n2
+  }
+  else if (opType == 'mod' ){
+    result = n1%n2
+  }
+  updateResult(result)
+}
+
+function changeSign(e){
+  n1 = parseFloat(display.textContent)*-1
+  result = n1
+  n2 = NaN
+  display.textContent = result
+}
+
+function updateResult(result) {
+  console.log('result')
+  n1 = result
+  n2 = NaN
+  display.textContent = result
 }
 
 function opSelect(e){
-  display.textContent = ''
   operatorSelected = true
   console.log(e.target.id)
+  display.value = undefined
+  console.log(display.value)
   opType = e.target.id
-  console.log(opType)
+ 
 }
 
 function numSelect (e) {
-    display.textContent = display.textContent +  e.target.innerHTML
     if (operatorSelected == false){
-      n1 = parseFloat(display.textContent)
+      display.textContent = display.textContent +  e.target.innerHTML
+      display.value = parseFloat(display.textContent)
+      n1 = display.value
       console.log("n1: "+n1)
     }
+
+    else if (display.value == undefined){
+      console.log(e.target.innerHTML)
+      display.textContent = e.target.innerHTML
+      display.value = parseFloat(display.textContent)
+      n2 = display.value
+      console.log("n2: "+n2)
+    
+    }
     else {
-      n2 = parseFloat(display.textContent)
+      console.log("display value" + display.value)
+      display.textContent = display.textContent +  e.target.innerHTML
+      display.value = parseFloat(display.textContent)
+      n2 = display.value
       console.log("n2: "+n2)
     }
   }
